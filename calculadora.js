e1fecc93d0186fdcc822d4cccdb1cebad45f6846
Vitor Passos <vitaopassosrodriguessantos@gmail.com>
2025-07-01 19:44:06 -0300

    Ajuste do código

--- calculadora.js
+++ calculadora.js
@@ -1,17 +1,17 @@
-function somar(n1,n2) {
+function somar(n1, n2) {
     return n1 + n2;
 }
 
-function subtrair(n1,n2) {
+function subtrair(n1, n2) {
     return n1 - n2;
 }
 
-function multiplicar(n1,n2) {
+function multiplicar(n1, n2) {
     return n1 * n2
 }
 
-function dividir(n1,n2) {
-    if (n2 != 0){
+function dividir(n1, n2) {
+    if (n2 != 0) {
         return n1 / n2;
     } else {
         alert('Não é possível dividir por zero!')
